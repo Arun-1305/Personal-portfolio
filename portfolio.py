ca9c08fee68ca896b60838d3e184de0ee1e0d29e
Arun <k33427471@gmail.com>
2026-05-16 08:16:32 +0530

    My personal portfolio site

---
+++ portfolio.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+app=Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def portfolio():
+    if request.method=="POST":
+        if request.form["text"]=="Arun Sharma" and request.form["password"]=="1305":
+         return render_template("portfolio.html")
+        else:
+         return "Either the password or the username is incorrect-_-"
+        
+    return render_template("home.html")
+
+if  __name__ == "__main__":
+     app.run(debug=True)
